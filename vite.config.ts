1545695091b16caed01a9d08dfe73a191e2bca87
moritalous <moritalous@gmail.com>
2025-05-25 04:28:50 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,24 +12,32 @@ export default defineConfig({
     rollupOptions: {
       // Externalize node built-in modules
       external: [
-        'node:child_process', 'node:path', 'node:url', 'path', 'fs', 'os', 
-        'crypto', 'child_process', 'module', 'assert', 'util', 'fs/promises',
-        'stream', 'url', 'net', 'https', 'constants', 'zlib',
-        /aws-cdk-lib.*/,
-        /@aws-amplify\/.*/
+        'node:child_process',
+        'node:path',
+        'node:url',
+        'path',
+        'fs',
+        'os',
+        'crypto',
+        'child_process',
+        'module',
+        'assert',
+        'util',
+        'fs/promises',
+        'stream',
+        'url',
+        'net',
+        'https',
+        'constants',
+        'zlib',
       ],
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return 'vendor';
           }
-        }
-      }
-    }
-    // // Limit memory usage
-    // target: 'es2015',
-    // minify: true,
-    // sourcemap: true,
-    // chunkSizeWarningLimit: 2000
+        },
+      },
+    },
   },
 });
